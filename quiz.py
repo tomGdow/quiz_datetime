f2709c05bb9e611712c344031b877f227b9a97d9
tomgdow <thomasgdowling@gmail.com>
2021-10-03 09:47:22 +0100

    fix d.answers/d.codes unexplained switch in quiz.py

--- quiz.py
+++ quiz.py
@@ -5,8 +5,8 @@ import functions_quiz as f
 import data_quiz as d
 
 if __name__ == '__main__':
-
     problem_list = [p.problem_maker(x) for x in d.codes.keys()]
+    # problem_list = [p.problem_maker(x) for x in d.answers.keys()]
     random_problem_list = random.sample(problem_list, k=len(problem_list)) # k = probem_number
     question_counter = f.counter(len(problem_list))
     f.display_problem(random_problem_list[0],question_counter, len(problem_list))
